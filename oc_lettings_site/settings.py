2417e9b1200a6724cb5b5362a58786299c642707
hgxv <kevguillot99@gmail.com>
2024-01-12 13:03:21 +0100

    update hosts settings

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["0.0.0.0", "https://oc-lettings-13-404d95b0521a.herokuapp.com/", "localhost"]
 
 # Application definition
 
